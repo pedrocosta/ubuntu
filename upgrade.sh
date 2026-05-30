0880c66773b6425e407f58467233aa6bd802b857
Pedro Costa <eng.pedrocosta@gmail.com>
2026-05-30 18:06:30 +0100

    Enhance upgrade script with nix commands
    Added commands for nix profile upgrade and garbage collection.

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# upgrade
 sudo apt full-upgrade --update --autoremove -y
-sudo determinate-nixd upgrade
 flatpak upgrade -y
 sudo snap refresh
+sudo determinate-nixd upgrade
 nix profile upgrade --all
+# clean
 nix-collect-garbage -d
